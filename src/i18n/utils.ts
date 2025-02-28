0abd3db2c1906e9470506a1a7e20646c77209630
Emiliano Salgado Martínez <sanjixenon553@outlook.com>
2025-02-28 11:10:51 -0500

    implementacion de la funcionalidad del toggle

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -4,7 +4,7 @@ const defaultLang = 'es';
 
 export function useTranslations(lang: keyof typeof labels){
     return function translate(key: keyof typeof labels [typeof defaultLang]){
-        return labels[lang][key] || labels[defaultLang][key];
-        // return (labels[lang] as typeof labels[typeof defaultLang])[key] || labels[defaultLang][key];
+        // return labels[lang][key] || labels[defaultLang][key];
+        return (labels[lang] as typeof labels[typeof defaultLang])[key] || labels[defaultLang][key];
     }
 }
